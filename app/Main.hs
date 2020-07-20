48a9bf1877a1682e53a097e5b80eecb5c0d895c3
kevin meyers <kevinm1776@gmail.com>
2020-07-19 23:11:09 -0400

    Added parsec, and validity checks.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,22 @@
 module Main where
 
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
 import Lib
 
 main :: IO ()
 main = do
-    putStrLn "Who are you?"
-    name <- getLine
-    putStrLn $ "Hello, " ++ name
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
