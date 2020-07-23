cc9ce3889c677a4d634dc16b368d00263f59a801
kevin meyers <kevinm1776@gmail.com>
2020-07-22 21:44:01 -0400

    Added number parsing for different bases!

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
-import Text.ParserCombinators.Parsec.Number (binary)
 import System.Environment
+import Data.Char (isDigit, digitToInt)
 
 import Lib
 
@@ -34,7 +34,7 @@ spaces = skipMany1 space
 parseString :: Parser LispVal
 parseString = do
     char '"'
-    x <- many $ noneOf "\"" <|> (oneOf "\\" >> oneOf "\"")
+    x <- many $ (oneOf "\\" >> oneOf "\"") <|> noneOf "\""
     char '"'
     return $ String x
 
@@ -48,13 +48,49 @@ parseAtom = do
         "#f" -> Bool False
         _ -> Atom atom
 
+convertBase :: String -> Int -> (Char -> Int) -> Int
+convertBase xs base convF = foldr (\(x, i) acc -> convF x * base^i + acc) 0 $ zip xs [l-1, l-2 .. 0]
+    where
+        l = length xs
+
+
+hexToDecimal :: Char -> Int
+hexToDecimal c
+    | isDigit c = digitToInt c
+hexToDecimal 'A' = 10
+hexToDecimal 'B' = 11
+hexToDecimal 'C' = 12
+hexToDecimal 'D' = 13
+hexToDecimal 'E' = 14
+hexToDecimal 'F' = 15
+
+parseHex :: Parser Int
+parseHex = do
+    string "#x"
+    xs <- many1 hexDigit
+    return $ convertBase xs 16 hexToDecimal
+
+parseBin :: Parser Int
+parseBin = do
+    string "#b"
+    xs <- many1 (oneOf "01")
+    return $ convertBase xs 2 digitToInt
+
+parseOct :: Parser Int
+parseOct = do
+    string "#o"
+    xs <- many1 octDigit
+    return $ convertBase xs 8 digitToInt
+
+parseDec :: Parser Int
+parseDec = do
+    optional $ string "#d"
+    xs <- many1 digit
+    return $ convertBase xs 10 digitToInt
+
 parseNumber :: Parser LispVal
-parseNumber = string "#x" >> many1 hexDigit
-            <|> string "#o" >> many1 octDigit
-            <|> string "#b" >> many1 binary
-Number . read <$> many1 digit
+parseNumber = Number . fromIntegral <$> (parseOct <|> parseHex <|> parseBin <|> parseDec)
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
-        <|> parseNumber
